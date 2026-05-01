711210f8f94f83f0546b565c56546ee4272be600
Tim-connor <connor.timmins@spartans.ut.edu>
2026-04-30 21:28:24 -0400

    Update bomb_configs.py

--- bomb_configs.py
+++ bomb_configs.py
@@ -1,136 +1,180 @@
 #################################
 # CSC 102 Defuse the Bomb Project
-# Configuration file
+# Main program
 # Team: 
 #################################
 
-# constants
-DEBUG = False        # debug mode?
-RPi = False           # is this running on the RPi?
-SHOW_BUTTONS = False # show the Pause and Quit buttons on the main LCD GUI?
-COUNTDOWN = 300      # the initial bomb countdown value (seconds)
-NUM_STRIKES = 5      # the total strikes allowed before the bomb "explodes"
-NUM_PHASES = 4       # the total number of initial active bomb phases
+# import the configs
+from bomb_configs import *
+# import the phases
+from bomb_phases import *
 
-# imports
-from random import randint, shuffle, choice
-from string import ascii_uppercase
-if (RPi):
-    import board
-    from adafruit_ht16k33.segments import Seg7x4
-    from digitalio import DigitalInOut, Direction, Pull
-    from adafruit_matrixkeypad import Matrix_Keypad
+###########
+# functions
+###########
+# generates the bootup sequence on the LCD
+def bootup(n=0):
+    gui._lscroll["text"] = boot_text.replace("\x00", "")
+    # configure the remaining GUI widgets
+    gui.setup()
+    # setup the phase threads, execute them, and check their statuses
+    if (RPi):
+        setup_phases()
+        check_phases()
 
-#################################
-# setup the electronic components
-#################################
-# 7-segment display
-# 4 pins: 5V(+), GND(-), SDA, SCL
-#         ----------7SEG---------
-if (RPi):
-    i2c = board.I2C()
-    component_7seg = Seg7x4(i2c)
-    # set the 7-segment display brightness (0 -> dimmest; 1 -> brightest)
-    component_7seg.brightness = 0.5
+# sets up the phase threads
+def setup_phases():
+    global timer, keypad, wires, button, toggles
+    
+    # setup the timer thread
+    timer = Timer(component_7seg, COUNTDOWN)
+    # bind the 7-segment display to the LCD GUI so that it can be paused/unpaused from the GUI
+    gui.setTimer(timer)
+    # setup the keypad thread
+    keypad = Keypad(component_keypad, keypad_target)
+    # setup the jumper wires thread
+    wires = Wires(component_wires, wires_target)
+    # setup the pushbutton thread
+    button = Button(component_button_state, component_button_RGB, button_target, button_color, timer)
+    # bind the pushbutton to the LCD GUI so that its LED can be turned off when we quit
+    gui.setButton(button)
+    # setup the toggle switches thread
+    toggles = Toggles(component_toggles, toggles_target)
 
-# keypad
-# 8 pins: 10, 9, 11, 5, 6, 13, 19, NA
-#         -----------KEYPAD----------
-if (RPi):
-    # the pins
-    keypad_cols = [DigitalInOut(i) for i in (board.D10, board.D9, board.D11)]
-    keypad_rows = [DigitalInOut(i) for i in (board.D5, board.D6, board.D13, board.D19)]
-    # the keys
-    keypad_keys = ((1, 2, 3), (4, 5, 6), (7, 8, 9), ("*", 0, "#"))
+    # start the phase threads
+    timer.start()
+    keypad.start()
+    wires.start()
+    button.start()
+    toggles.start()
 
-    component_keypad = Matrix_Keypad(keypad_rows, keypad_cols, keypad_keys)
+# checks the phase threads
+def check_phases():
+    global active_phases
+    
+    # check the timer
+    if (timer._running):
+        # update the GUI
+        gui._ltimer["text"] = f"Time left: {timer}"
+    else:
+        # the countdown has expired -> explode!
+        # turn off the bomb and render the conclusion GUI
+        turn_off()
+        gui.after(100, gui.conclusion, False)
+        # don't check any more phases
+        return
+    # check the keypad
+    if (keypad._running):
+        # update the GUI
+        gui._lkeypad["text"] = f"Combination: {keypad}"
+        # the phase is defused -> stop the thread
+        if (keypad._defused):
+            keypad._running = False
+            active_phases -= 1
+        # the phase has failed -> strike
+        elif (keypad._failed):
+            strike()
+            # reset the keypad
+            keypad._failed = False
+            keypad._value = ""
+    # check the wires
+    if (wires._running):
+        # update the GUI
+        gui._lwires["text"] = f"Wires: {wires}"
+        # the phase is defused -> stop the thread
+        if (wires._defused):
+            wires._running = False
+            active_phases -= 1
+        # the phase has failed -> strike
+        elif (wires._failed):
+            strike()
+            # reset the wires
+            wires._failed = False
+    # check the button
+    if (button._running):
+        # update the GUI
+        gui._lbutton["text"] = f"Button: {button}"
+        # the phase is defused -> stop the thread
+        if (button._defused):
+            button._running = False
+            active_phases -= 1
+        # the phase has failed -> strike
+        elif (button._failed):
+            strike()
+            # reset the button
+            button._failed = False
+    # check the toggles
+    if (toggles._running):
+        # update the GUI
+        gui._ltoggles["text"] = f"Toggles: {toggles}"
+        # the phase is defused -> stop the thread
+        if (toggles._defused):
+            toggles._running = False
+            active_phases -= 1
+        # the phase has failed -> strike (toggles don't fail, just keep trying)
+        elif (toggles._failed):
+            strike()
+            # reset the toggles
+            toggles._failed = False
 
-# jumper wires
-# 10 pins: 14, 15, 18, 23, 24, 3V3, 3V3, 3V3, 3V3, 3V3
-#          -------JUMP1------  ---------JUMP2---------
-# the jumper wire pins
-if (RPi):
-    # the pins
-    component_wires = [DigitalInOut(i) for i in (board.D14, board.D15, board.D18, board.D23, board.D24)]
-    for pin in component_wires:
-        # pins are input and pulled down
-        pin.direction = Direction.INPUT
-        pin.pull = Pull.DOWN
+    # note the strikes on the GUI
+    gui._lstrikes["text"] = f"Strikes left: {strikes_left}"
+    # too many strikes -> explode!
+    if (strikes_left == 0):
+        # turn off the bomb and render the conclusion GUI
+        turn_off()
+        gui.after(1000, gui.conclusion, False)
+        # stop checking phases
+        return
 
-# pushbutton
-# 6 pins: 4, 17, 27, 22, 3V3, 3V3
-#         -BUT1- -BUT2-  --BUT3--
-if (RPi):
-    # the state pin (state pin is input and pulled down)
-    component_button_state = DigitalInOut(board.D4)
-    component_button_state.direction = Direction.INPUT
-    component_button_state.pull = Pull.DOWN
-    # the RGB pins
-    component_button_RGB = [DigitalInOut(i) for i in (board.D17, board.D27, board.D22)]
-    for pin in component_button_RGB:
-        # RGB pins are output
-        pin.direction = Direction.OUTPUT
+    # the bomb has been successfully defused!
+    if (active_phases == 0):
+        # turn off the bomb and render the conclusion GUI
+        turn_off()
+        gui.after(100, gui.conclusion, True)
+        # stop checking phases
+        return
+
+    # check the phases again after a slight delay
+    gui.after(100, check_phases)
+
+# handles a strike
+def strike():
+    global strikes_left
+    
+    # note the strike
+    strikes_left -= 1
+
+# turns off the bomb
+def turn_off():
+    # stop all threads
+    timer._running = False
+    keypad._running = False
+    wires._running = False
+    button._running = False
+    toggles._running = False
+
+    # turn off the 7-segment display
+    component_7seg.blink_rate = 0
+    component_7seg.fill(0)
+    # turn off the pushbutton's LED
+    for pin in button._rgb:
         pin.value = True
 
-# toggle switches
-# 3x3 pins: 12, 16, 20, 21, 3V3, 3V3, 3V3, 3V3, GND, GND, GND, GND
-#           -TOG1-  -TOG2-  --TOG3--  --TOG4--  --TOG5--  --TOG6--
-if (RPi):
-    # the pins
-    component_toggles = [DigitalInOut(i) for i in (board.D12, board.D16, board.D20, board.D21)]
-    for pin in component_toggles:
-        # pins are input and pulled down
-        pin.direction = Direction.INPUT
-        pin.pull = Pull.DOWN
+######
+# MAIN
+######
 
-###########
-# functions to generate targets for toggles/wires/keypad/Button
-###########
-def genSerial():
-    # Create your own logic of making a serial number (if needed)
-    # TODO
-    return "B026DES"
+# initialize the LCD GUI
+window = Tk()
+gui = Lcd(window)
 
-def genTogglesTarget():
-    return 0b1101  # = 13 in decimal
-    
+# initialize the bomb strikes and active phases (i.e., not yet defused)
+strikes_left = NUM_STRIKES
+active_phases = NUM_PHASES
 
-def genWiresTarget():
-    # Create your own logic of making a target number for wires
-    # TODO
-    return 5
-# generates the keypad combination from a keyword and rotation key
-def genKeypadTarget():
-    # Create your own logic of making a keypad combination number if needed
-    # TODO
-    return "26863"
+# "boot" the bomb
+gui.after(100, bootup)
 
-# generate the color of the pushbutton (which determines how to defuse the phase)
-button_color = choice(["R", "G", "B"])
-
-def genButtonTarget():
-    # TODO
-    global button_color
-    # Create your own logic of making a Button target
-    # appropriately set the target (R is None)
-    b_target = None
-    # G is the first numeric digit in the serial number
-    if (button_color == "G"):
-        b_target = [ n for n in serial if n.isdigit() ][0]
-    # B is the last numeric digit in the serial number
-    elif (button_color == "B"):
-        b_target = [ n for n in serial if n.isdigit() ][-1]
-
-    return b_target
-
-###############################
-serial = genSerial()
-toggles_target = genTogglesTarget()
-wires_target = genWiresTarget()
-keypad_target = genKeypadTarget()
-button_target = genButtonTarget()
-
-# set the bomb's LCD bootup text
-boot_text = f"*Add your own text here specific to your bomb*\n"\
-            f"*Serial number: {serial}\n"\
-            
+# display the LCD GUI
+window.mainloop()
